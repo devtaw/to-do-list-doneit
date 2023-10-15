3e08290d5bab09a9b805438a5d2604b0a4918544
Taw Simões <tawm.dev@gmail.com>
2023-10-15 19:47:48 -0300

    :sparkles: feat: Cria função que recebe descrição e adiciona novo objeto com essa descrição na lista de To Dos

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 import "./App.css";
 import { TodoItem } from "./components/TodoItem";
 import { AddTodoInput } from "./components/AddTodoInput";
-
 function App() {
   const [todos, setTodos] = useState([]);
+
+  function handleAddNewTodoItem(description) {
+    setTodos((todos) => [
+      ...todos,
+      {
+        description,
+        isCompleted: false,
+      },
+    ]);
+  }
 
   return (
     <>
       <h1>Done It - Lista de Tarefas</h1>
-      <AddTodoInput />
+
+      <AddTodoInput onAddItem={handleAddNewTodoItem} />
+
       {todos.map((todo) => (
         <TodoItem description={todo.description} />
       ))}
