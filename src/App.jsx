8d938f5cade36437be387ae62c6ebdbbe08d3fda
Taw Simões <tawm.dev@gmail.com>
2023-10-15 19:08:33 -0300

    :sparkles: feat: Cria componente Item do To Do List

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
+import { TodoItem } from "./components/TodoItem";
 
 const FAKE_DATA = [
   {
@@ -15,15 +17,16 @@ const FAKE_DATA = [
     isCompleted: false,
   },
 ];
-
 function App() {
   const [todos, setTodos] = useState(FAKE_DATA);
-
   return (
     <>
-      <h1>Done It - Lista de Tarefas</h1>
+      <h1>Todo List</h1>
+
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </>
   );
 }
-
 export default App;
