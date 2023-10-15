d438392946f308bdbbc072ea2c92e02fc5a96fd2
Taw Simões <tawm.dev@gmail.com>
2023-10-15 19:34:04 -0300

    :sparkles: feat: Atualiza texto no placeholder

--- src/components/AddTodoInput.jsx
+++ src/components/AddTodoInput.jsx
@@ -1,9 +1,8 @@
 import { Button, Container, TextInput } from "./AddTodoInput.styled";
-
 export function AddTodoInput() {
   return (
     <Container>
-      <TextInput placeholder="Add a new todo..." />
+      <TextInput placeholder="Adicione uma nova tarefa..." />
       <Button>Add</Button>
     </Container>
   );
