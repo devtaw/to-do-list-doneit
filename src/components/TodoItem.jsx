64c7970c87f62979c83d5cdb45a5c213c3c6c69d
Taw Simões <tawm.dev@gmail.com>
2023-10-15 19:55:58 -0300

    :sparkles: feat: Adiciona estado para identificar se o usuário está editando o To Do

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-import { CheckboxInput, Container, Description } from "./TodoItem.styled";
+import { CheckboxInput, Container, Description, TextInput } from "./TodoItem.styled";
 
-/**
- * @typedef {Object} TodoItemProps
- * @property {string} description
- *
- * @param {TodoItemProps} props
- */
+// @typedef {Object} TodoItemProps
+// @property {string} description
+// @param {TodoItemProps} props
+
 export function TodoItem(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   const [isCompleted, setIsCompleted] = useState(false);
+
+  function handleEditClick() {
+    // atualiza o estado de isEditing para o valor true
+    // para que o input de texto seja exibido
+
+    setIsEditing(true);
+  }
+
+  function handleSaveClick() {
+    // atualiza o estado de isEditing para o valor false
+    // para que o input de texto seja escondido
+
+    setIsEditing(false);
+  }
 
   return (
     <Container>
@@ -21,12 +35,12 @@ export function TodoItem(props) {
         />
       </div>
 
-      <div className="description">
-        <Description isCompleted={isCompleted}>{props.description}</Description>
-      </div>
+      <div className="description">{!isEditing && <Description isCompleted={isCompleted}>{props.description}</Description>}</div>
 
       <div className="actions">
-        <button className="edit">Edit</button>
+        {!isEditing && <button onClick={handleEditClick}>Edit</button>}
+
+        {isEditing && <button onClick={handleSaveClick}>Save</button>}
       </div>
     </Container>
   );
