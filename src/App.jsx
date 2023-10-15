87afab7f0895b727b1fca7c58344df02c3eca986
Taw Simões <tawm.dev@gmail.com>
2023-10-15 19:41:35 -0300

    :sparkles: feat: Remove dados fake

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,13 @@ import "./App.css";
 import { TodoItem } from "./components/TodoItem";
 import { AddTodoInput } from "./components/AddTodoInput";
 
-const FAKE_DATA = [
-  {
-    description: "Learn React",
-    isCompleted: false,
-  },
-  {
-    description: "Learn Vite",
-    isCompleted: false,
-  },
-  {
-    description: "Learn Tailwind",
-    isCompleted: false,
-  },
-];
 function App() {
-  const [todos, setTodos] = useState(FAKE_DATA);
+  const [todos, setTodos] = useState([]);
+
   return (
     <>
-      <h1>Todo List</h1>
-
+      <h1>Done It - Lista de Tarefas</h1>
       <AddTodoInput />
-
       {todos.map((todo) => (
         <TodoItem description={todo.description} />
       ))}
